144536539e1cde024546b440b1cb9e71c7c98d2c
Dhruveshp23 <122923489+Dhruveshp23@users.noreply.github.com>
2023-01-31 14:46:26 -0500

    Update Main.java

--- LM 1- Text Adventure Items.iml/src/Main.java
+++ LM 1- Text Adventure Items.iml/src/Main.java
@@ -1,13 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        Item goldBar = new Gold(10);
-        Item magicSword = new Sword();
-        Item softPillow = new Pillow();
-        Item bigMace = new Mace();
 
+//Item objects        
+        Item goldBar = new Gold(10);
         System.out.println(goldBar);
+
+        Item magicSword = new Sword();
         System.out.println(magicSword);
+
+        Item softPillow = new Pillow();
         System.out.println(softPillow);
+
+        Item bigMace = new Mace();
         System.out.println(bigMace);
+
+// Enemy objects
+        Enemy greenOgre = new Ogre();
+        System.out.println(greenOgre);
+
+        Enemy redOgre = new Ogre();
+        System.out.println(redOgre);
+
+        Enemy slowZombie = new Zombie();
+        System.out.println(slowZombie);
+
+        Enemy fastZombie = new Zombie();
+        System.out.println(fastZombie);
+
+        Enemy spotDog = new Dog();
+        System.out.println(spotDog);
+
+        Enemy bigSpider = new GiantSpider();
+        System.out.println(bigSpider);
+
+        Enemy poisonLizard = new Lizards();
+        System.out.println(poisonLizard);
+
+
     }
 }
